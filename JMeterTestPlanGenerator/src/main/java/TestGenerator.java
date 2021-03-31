6c5d64f45da2d587559aa08d3d565ca87df06d3e
Agostino <agostino.delucia@hotmail.it>
2021-03-31 18:57:01 +0200

    .

--- JMeterTestPlanGenerator/src/main/java/TestGenerator.java
+++ JMeterTestPlanGenerator/src/main/java/TestGenerator.java
@@ -183,9 +183,9 @@ public class TestGenerator {
                                 "      <kg.apc.jmeter.timers.VariableThroughputTimer guiclass=\"kg.apc.jmeter.timers.VariableThroughputTimerGui\" testclass=\"kg.apc.jmeter.timers.VariableThroughputTimer\" testname=\"shapingTimer\" enabled=\"" + enabled + "\">\n" +
                                 "        <collectionProp name=\"load_profile\">\n" +
 
-                                "          <collectionProp name=\"176726940\">\n" +
-                                "            <stringProp name=\"1567\">" + rps +"</stringProp>\n" +
-                                "            <stringProp name=\"1567\">" + rps + "</stringProp>\n" +
+                                "          <collectionProp name=\"-2002903193\">\n" +
+                                "            <stringProp name=\"49\">1</stringProp>\n" +
+                                "            <stringProp name=\"1572\">15</stringProp>\n" +
                                 "            <stringProp name=\"1515111\">1800</stringProp>\n" +
                                 "          </collectionProp>\n" +
 
